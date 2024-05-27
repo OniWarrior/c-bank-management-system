8d10e9977f0aa0a17e5a0b6969a8b20aed1bfdb7
Stephen Aranda <aranda.stephen88@gmail.com>
2024-05-27 16:24:46 -0500

    defined an account struct for linked list db

---
+++ linkedlist.h
@@ -0,0 +1,27 @@
+// Author: Stephen Aranda
+// File  : linkedlist.h
+// Description : This is a header file to define a linked list for
+//             : the bank management app
+
+#ifndef __LINKED_LIST__
+#define __LINKED_LIST__
+#include <stdio.h>
+#include <stdlib.h>
+
+// struct that defines a account node in linked list that
+// simulates a persistent database for bank app
+typedef struct Account
+{
+    char first_name[50];
+    char last_name[50];
+    char email[50];
+    char username[20];
+    char password[15];
+    float balance = 0.00;
+
+    // pointer to next node
+    struct Account *next;
+
+} Account;
+
+#endif
