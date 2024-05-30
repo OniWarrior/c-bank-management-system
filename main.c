06dfd8a337c68b0d58d4701bce9973ba3c555b69
Stephen Aranda <aranda.stephen88@gmail.com>
2024-05-30 17:41:03 -0500

    added title to console screen

---
+++ main.c
@@ -0,0 +1,12 @@
+// Author: stephen aranda
+// file  : main.cpp
+// date  : 5/30/2024
+// desc  : entry point for bank management app
+#include "hub.h"
+
+int main()
+{
+    printf_s("************Welcome to Bank Management App***********************\n\n");
+
+    return 0;
+}
