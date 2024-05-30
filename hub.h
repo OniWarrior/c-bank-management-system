4d25f6ad8e9ebc4f50c37908b47c454aa5ce17a6
Stephen Aranda <aranda.stephen88@gmail.com>
2024-05-30 17:41:19 -0500

    added blank hub function

---
+++ hub.h
@@ -0,0 +1,15 @@
+// Author: stephen aranda
+// file  : hub.c
+// date  : 5/30/2024
+// desc  : file for the menu hub
+
+#ifndef __BANK_OPS__
+#define __BANK_OPS__
+#include <stdio.h>
+#include <stdlib.h>
+
+void hub()
+{
+}
+
+#endif
